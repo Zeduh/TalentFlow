1687e6da68afbfc2b76c1edd4be83ba3c4065f9f
José Eduardo Cury <jeduardocury@gmail.com>
2025-10-12 03:06:03 -0300

    test: configura ambiente de testes do frontend com Jest

---
+++ frontend/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': ['@swc/jest', {}],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+};
+
+export default config;
