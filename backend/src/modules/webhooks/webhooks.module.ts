93a798fe8787691d3089714bf11f83e58a889d00
José Eduardo Cury <jeduardocury@gmail.com>
2025-10-13 01:07:41 -0300

    fix(webhook): injeta InterviewService via InterviewModule no WebhooksModule

--- backend/src/modules/webhooks/webhooks.module.ts
+++ backend/src/modules/webhooks/webhooks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhooksController } from './webhooks.controller';
 import { WebhooksService } from './webhooks.service';
+import { InterviewModule } from '../interviews/interview.module';
 
 @Module({
+  imports: [InterviewModule],
   controllers: [WebhooksController],
   providers: [WebhooksService],
   exports: [WebhooksService],
