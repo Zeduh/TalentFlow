c0b2183bd16a6a04f3abf3910d3491e30febae99
José Eduardo <jeduardocury@gmail.com>
2025-10-14 04:44:13 -0300

    chore: ajusta provider global para contexto de autenticação e toasts

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 import { AuthProvider } from '../components/AuthProvider'
+import { Toaster } from 'react-hot-toast';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -22,6 +23,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         {children}
+        <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
       </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
